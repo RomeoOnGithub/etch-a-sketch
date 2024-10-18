30de6bdd9aabdc1859303deb47ff244f6a320a6c
RomeoOnGithub <168174189+RomeoOnGithub@users.noreply.github.com>
2024-10-18 14:13:50 +0200

    centered 16x16 grid of square divs

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const grid = document.querySelector('#grid');
+let gridSize = 16; //default grid size
+
+function createGrid(size) {
+//create cells
+    for (i = 0; i < size * size; i++) {
+        const newCell = document.createElement('div');
+        newCell.classList.add('cell-style');
+        grid.appendChild(newCell);    
+    }
+
+//dynamically style cells to fit with 'grid' container (dynamic CSS)
+    const cellSize = 100 / size;
+    const cells = document.querySelectorAll('.cell-style');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].style.width = `${cellSize}%`;
+        cells[i].style.height = `${cellSize}%`;
+    }
+}
+
+createGrid(gridSize);
