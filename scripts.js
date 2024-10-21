8d6225bc919fdb177a68bfffe60d4efbc1e9c374
RomeoOnGithub <168174189+RomeoOnGithub@users.noreply.github.com>
2024-10-21 15:02:13 +0200

    'hover' effect to black

--- scripts.js
+++ scripts.js
@@ -9,12 +9,17 @@ function createGrid(size) {
         grid.appendChild(newCell);    
     }
 
-//dynamically style cells to fit with 'grid' container (dynamic CSS)
+//dynamically format cells to fit with 'grid' container (dynamic CSS)
     const cellSize = 100 / size;
     const cells = document.querySelectorAll('.cell-style');
     for (let i = 0; i < cells.length; i++) {
         cells[i].style.width = `${cellSize}%`;
         cells[i].style.height = `${cellSize}%`;
+
+        //'hover' effect (change color)
+        cells[i].addEventListener("mouseenter", function() {
+            cells[i].classList.add('cell-color');
+        })
     }
 }
 
